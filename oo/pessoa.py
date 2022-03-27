f1e8910b321297fd0fa3600caff57cce42913c03
DouglasPortela0403 <portela.douglas@gmail.com>
2022-03-27 13:40:14 -0400

    implementação do carro, motor e direção.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,23 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    olhos = 2
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos =list(filhos)
+
     def cumprimentar(self):
         return 'Olá'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls}, {olhos}'
+
+
 if __name__ == '__main__':
-    p = Pessoa('Mariane')
+    p = Pessoa(nome='Mariane')
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
     print(p.nome)
@@ -14,5 +26,10 @@ if __name__ == '__main__':
     p.idade = 35
     print(p.nome)
     print(p.idade)
+    print(Pessoa.olhos)
+    print(p.__dict__)
+    print(Pessoa.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe())
+
 
 
